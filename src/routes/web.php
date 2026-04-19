83b09bc96f8815b2b4cf100f6a4e0b017bd97f20
nico <yuuuuuuuu.13@gmail.com>
2026-04-19 22:59:54 +0900

    update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChildController;
+use App\Http\Controllers\RewardController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\CategoryController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/child/index', [ChildController::class, 'index'])->name('child.index');
+Route::post('/child/store', [ChildController::class, 'store'])->name('child.store');
+
+Route::post('/category/store', [CategoryController::class, 'store'])->name('category.store');
+
+Route::post('/task/store', [TaskController::class, 'store'])->name('task.store');
+
+Route::post('/reward/store', [RewardController::class, 'store'])->name('reward.store');
